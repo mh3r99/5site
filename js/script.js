4b28d9de984b293bc8997527a61869ef2c198319
mh3r99 <66919283+mh3r99@users.noreply.github.com>
2020-09-10 22:43:23 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+menu.onclick = function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "top-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "top-nav";
+  }
+};
